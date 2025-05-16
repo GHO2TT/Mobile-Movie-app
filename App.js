fa742757df5b7014b07f60119e1286c1d1be015b
IYKE <ndavidabjc@gmail.com>
2025-05-16 20:46:56 +0100

    fix

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigation from "./components/navigation";
+import "./global.css";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <StatusBar style="auto" />
+  //     {/* <Text className="text-blue-500 text-lg font-bold">
+  //       Open up App.js to start working on your app!
+  //     </Text> */}
+  //     <AppNavigation />
+  //   </View>
+  // );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/HomeScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
